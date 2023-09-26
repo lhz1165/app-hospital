ae950a4bc97feae8bf7d0da96b519e5603e49ec3
lhz <2190732276@qq.com>
2023-09-26 10:15:18 +0800

    manage

---
+++ hospital-system/hospital-manage/src/main/java/jw/hmanage/hospitalmanage/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package jw.hmanage.hospitalmanage.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MybatisPlus配置类
+ *
+ * @author qy
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("jw.hmanage.hospitalmanage.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//如果配置多个插件,切记分页最后添加
+        return interceptor;
+    }
+
+}
---
+++ hospital-system/hospital-manage/src/main/java/jw/hmanage/hospitalmanage/mapper/HospitalSetMapper.java
@@ -0,0 +1,10 @@
+package jw.hmanage.hospitalmanage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import jw.hmanage.hospitalmanage.model.HospitalSet;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
