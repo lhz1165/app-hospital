9cdb4abeb56a4a9fef9d6084a497ac1d50d9371d
lhz <2190732276@qq.com>
2023-09-27 15:54:56 +0800

    dev prod环境

--- hospital-system/hosp-service/src/main/java/com/siro/yyds/hosp/ServiceHospApplication.java
+++ hospital-system/hosp-service/src/main/java/com/siro/yyds/hosp/ServiceHospApplication.java
@@ -1,8 +1,17 @@
 package com.siro.yyds.hosp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,11 +23,17 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.siro.yyds")
 @MapperScan("com.siro.yyds.hosp.mapper")
-public class ServiceHospApplication {
+@Slf4j
+public class ServiceHospApplication implements ApplicationListener<ApplicationStartedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+
+        log.info("start success,{}","http://localhost:8201/swagger-ui.html");
+    }
 
     public static void main(String[] args) {
-        System.out.println("http://localhost:8201/swagger-ui.html");
         SpringApplication.run(ServiceHospApplication.class, args);
     }
+
 
 }
--- hospital-system/init.sql
+++ hospital-system/init.sql
@@ -21,6 +21,24 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for dict
 -- ----------------------------
+
+CREATE TABLE `hospital_set` (
+                                `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+                                `hosname` varchar(100) DEFAULT NULL COMMENT '医院名称',
+                                `hoscode` varchar(30) DEFAULT NULL COMMENT '医院编号',
+                                `api_url` varchar(100) DEFAULT NULL COMMENT 'api基础路径',
+                                `sign_key` varchar(50) DEFAULT NULL COMMENT '签名秘钥',
+                                `contacts_name` varchar(20) DEFAULT NULL COMMENT '联系人',
+                                `contacts_phone` varchar(11) DEFAULT NULL COMMENT '联系人手机',
+                                `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '状态',
+                                `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '逻辑删除(1:已删除，0:未删除)',
+                                PRIMARY KEY (`id`),
+                                UNIQUE KEY `uk_hoscode` (`hoscode`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='医院设置表';
+
+
 DROP TABLE IF EXISTS `dict`;
 CREATE TABLE `dict`  (
   `id` bigint(20) NOT NULL DEFAULT 0 COMMENT 'id',
