c84b3212266cd462e5232f1c3bfb6867ad8701eb
laihz <2190732276@qq.com>
2023-10-25 16:21:17 +0800

    feature：vue

---
+++ hospital-system/front-user/hospital-user/pages/weixin/callback.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- header -->
+  <div></div>
+  <!-- footer -->
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    let token = this.$route.query.token;
+    let name = this.$route.query.name;
+    let openid = this.$route.query.openid;
+    // 调用父vue方法
+    window.parent["loginCallback"](name, token, openid);
+  },
+};
+</script>
